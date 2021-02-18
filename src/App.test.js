30c63aa024d200432852bdff3d2c93436c2859bb
bwolanczyk87 <bwolanczyk87@gmail.com>
2021-02-18 11:21:26 +0100

    update

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders hello world', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello, Miłosz!/i);
+  const linkElement = screen.getByText(/Hello, DevLab!/i);
   expect(linkElement).toBeInTheDocument();
 });
